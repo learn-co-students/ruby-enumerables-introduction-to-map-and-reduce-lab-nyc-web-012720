308eaa50516f219d1271d716d7bd71de235f4908
lior Shine <shinelior@gmail.com>
2020-01-08 15:42:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,119 @@
 # My Code here....
+def map_to_negativize(source_array)   
+ i = 0
+ new_array = []
+ while i < source_array.length do 
+ 
+ sum = source_array[i] * -1
+     i += 1 
+   new_array << sum 
+ end 
+ return new_array
+ end
+ 
+ def map_to_no_change(source_array)
+ i = 0
+ new_array = []
+ while i < source_array.length do 
+ 
+ sum = source_array[i]  
+     i += 1 
+   new_array << sum 
+ end 
+ return new_array
+ end 
+ 
+def  map_to_double(source_array)
+  i = 0
+ new_array = []
+ while i < source_array.length do 
+ 
+ sum = source_array[i] * 2 
+     i += 1 
+   new_array << sum 
+ end 
+ return new_array
+ 
+end
+def  map_to_square(source_array) 
+   i = 0
+ new_array = []
+ while i < source_array.length do 
+ 
+ sum = source_array[i] * source_array[i] 
+     i += 1 
+   new_array << sum 
+ end 
+ return new_array
+  
+ end  
+ 
+  def reduce_to_total(source_array)
+  source_array.reduce(:+)
+  # total = 0 
+  # i = 0  
+   #while i < source_array.length do 
+      
+            #  total  += source_array[i]
+ 
+          # i += 1  
+         #end 
+         #return total 
+      end 
+       
+ 
+ 
+ 
+ def reduce_to_total(source_array , starting_point)
+   #source_array.reduce{|x,starting_point|  starting_point + x}
+   
+   #source_array.reduce(:+)
+   
+   total = 0
+    i = 0
+
+ while i < source_array.length do 
+  
+      starting_point += source_array[i] 
+ 
+     i += 1 
+   
+ end 
+ return starting_point
+  
+ end
+ 
+ def  reduce_to_all_true(source_array)
+ i = 0  
+ while i < source_array.length  do 
+      if source_array[i] 
+        
+        i += 1  
+      else 
+      return   false
+      break!
+    end
+    #return true
+  end
+  return true
+ end
+ 
+ def  reduce_to_any_true(source_array)
+    
+        i = 0  
+   while i < source_array.length  do 
+      if source_array[i] != true
+        
+        i += 1  
+      else 
+      return   true
+      break!
+    end
+    
+  end
+  return false
+ end 
+        
+        
+
+ 
---
+++ my_code_two.rb
@@ -0,0 +1,15 @@
+def map_to_negativize(source_array)   
+ source_array.map{|x|  x*-1}
+ end
+ 
+ def map_to_no_change(source_array)
+ source_array.map{|x|  x}  
+ end
+def  map_to_double(source_array)
+  
+  source_array.map{|x|  x*2}
+end
+def  map_to_square(source_array) 
+   source_array.map{|x|  x*x} 
+ end  
+ 
