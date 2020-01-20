33eab3461910cfbaa11a2615aabc6b38d1652446
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-20 23:28:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,91 +1,151 @@
 def map_to_negativize(source_array)
   neg_array = []
-  source_array.length.times do |x|
-    neg_val = source_array[x] * -1
-    neg_array << neg_val
-  end
+  i = 0
+  while i < source_array.length do 
+    neg_array << source_array[i] * -1
+    i+=1 
+  end 
   neg_array
-end
-
+end 
 
 def map_to_no_change(source_array)
-  no_change_arr = []
-  source_array.length.times do |x|
-    same_val = source_array[x]
-    no_change_arr << same_val
-  end
-  no_change_arr
-end
-
+  no_change_array = []
+  i = 0 
+  while i < source_array.length do 
+    no_change_array << source_array[i]
+    i+=1 
+  end 
+  no_change_array
+end 
 
 def map_to_double(source_array)
-  double_arr = []
-  source_array.length.times do |x|
-    double_val = source_array[x] * 2 
-    double_arr << double_val
-  end
-  double_arr
-end
-
+  double_array = []
+  i = 0 
+  while i < source_array.length do 
+    double_array << source_array[i] * 2
+    i+=1 
+  end 
+  double_array
+end 
 
 def map_to_square(source_array)
-  sq_array = []
-  source_array.length.times do |x|
-    sq_val = source_array[x] ** 2 
-    sq_array << sq_val
-  end
-  sq_array
-end
-
-
-
-# *** REDUCE-LIKE ***
+  square_array = []
+  i = 0 
+  while i < source_array.length do
+    square_array << (source_array[i] ** 2)
+    i+=1 
+  end 
+  square_array
+end 
 
 def reduce_to_total(source_array, starting_point = 0)
-  total = 0
-  if starting_point 
-    source_array.length.times do |x|
-      starting_point = starting_point + source_array[x]
-      total = starting_point
-    end
-  end
-  total
+  i = 0 
+  while i < source_array.length do 
+    starting_point += source_array[i]
+    i+=1 
+  end 
+  total = starting_point
+  total 
 end
-
 
 def reduce_to_all_true(source_array)
-  all_true = []
-  source_array.length.times do |x|
-    if source_array[x]
-      all_true << source_array[x]
-    end
-  end
-  if all_true.length == source_array.length
-    all_true = true
-  else
-    all_true = false 
-  end
+  all_true = true 
+  i=0 
+  while i < source_array.length do
+    if !source_array[i]
+      all_true = false 
+    end 
+    i+=1 
+  end 
   all_true
-end
-
+end 
 
 def reduce_to_any_true(source_array)
-  any_true = []
-  
-  source_array.length.times do |x|
-    if source_array[x]
-      any_true << source_array[x]
+  any_true = false 
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i]
+      return any_true = true
     end
-  end
-  
-  if any_true.length == 0 
-    any_true = false 
-  else 
-    any_true = true
+    i+=1 
   end 
-  
   any_true
-end
+end 
+
+
+
+
+# # # *** ALTERNATIVES *** # # #
+
+
+# def map_to_negativize(source_array)
+#   neg_array = []
+#   source_array.length.times do |x|
+#     neg_val = source_array[x] * -1
+#     neg_array << neg_val
+#   end
+#   neg_array
+# end
+
+# def map_to_no_change(source_array)
+#   no_change_arr = []
+#   source_array.length.times do |x|
+#     same_val = source_array[x]
+#     no_change_arr << same_val
+#   end
+#   no_change_arr
+# end
+
+# def map_to_double(source_array)
+#   double_arr = []
+#   source_array.length.times do |x|
+#     double_val = source_array[x] * 2 
+#     double_arr << double_val
+#   end
+#   double_arr
+# end
+
+# def map_to_square(source_array)
+#   sq_array = []
+#   source_array.length.times do |x|
+#     sq_val = source_array[x] ** 2 
+#     sq_array << sq_val
+#   end
+#   sq_array
+# end
+
+# def reduce_to_total(source_array, starting_point = 0)
+#   acc = starting_point
+#   i = 0 
+#   while i < source_array.length do 
+#     next_val = source_array[i]
+#     acc = acc + next_val
+#     i+=1
+#   end 
+#   acc
+# end
+
+# def reduce_to_all_true(source_array)
+#   acc = true 
+#   i = 0 
+#   while i < source_array.length do 
+#     next_val = source_array[i]
+#     acc = acc && !!next_val
+#     i+=1 
+#   end 
+#   acc
+# end
+
+# def reduce_to_any_true(source_array)
+#   acc = false 
+#   i = 0 
+#   while i < source_array.length do 
+#     next_val = source_array[i]
+#     acc = acc || !!next_val
+#     i += 1 
+#   end 
+#   acc
+# end
 
 
 
---
+++ testing.rb
@@ -0,0 +1,6 @@
+sibs = ["yomi", "ana", "gweeji", "elz", "jen-slime", "g-diddy", "c-dawg"]
+
+
+new_sibs = sibs.map { |names| names + " taveras" }
+
+puts new_sibs 
