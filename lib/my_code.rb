9f1511a7c22506e1f3f230ca945aaade61463924
April Escobar <aprilfaith.e@gmail.com>
2020-01-22 03:54:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,10 +53,22 @@ end
 
 
 def reduce_to_all_true(source_array)
-  return source_array.all?()
+  # return source_array.all?()
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
 
 
 def reduce_to_any_true(source_array)
-  return source_array.any?()
+  # return source_array.any?()
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
